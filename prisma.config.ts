6357c9c34f4f079900a1fec170e3d24f05277ff5
Amir Momenian <nainemom@gmail.com>
2026-07-24 00:53:39 +0330

    build: make db url optional in prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,8 +3,6 @@ import { defineConfig } from 'prisma/config';
 
 const DB_URL = process.env.DB_POSTGRES_URL;
 
-if (!DB_URL) throw new Error('process.env.DB_POSTGRES_URL is not defined');
-
 export default defineConfig({
 	schema: 'src/relay/db/schema.prisma',
 	migrations: {
